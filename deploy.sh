92188df88f4b84bc0519c37d6e7fe974aace9735
Jing <jingchau.yang@aa.com>
2020-04-05 10:50:43 -0500

    Added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+docker build -t jyang116/multi-client:latest -t jyang116/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jyang116/multi-server:latest -t jyang116/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jyang116/multi-worker:latest -t jyang116/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push jyang116/multi-client:latest
+docker push jyang116/multi-server:latest
+docker push jyang116/multi-worker:latest
+docker push jyang116/multi-client:$SHA
+docker push jyang116/multi-server:$SHA
+docker push jyang116/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=jyang116/multi-cleint:$SHA
+kubectl set image deployments/server-deployment server=jyang116/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=jyang116/multi-worker:$SHA
